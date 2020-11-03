cd1e49d12098a1738f43ddbf86cd4db100494964
Jessica <jessicanolette@gmail.com>
2020-11-03 14:54:46 -0500

    Set up admin and models

--- budget/admin.py
+++ budget/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Project, Expense, Category
+
+
+admin.site.register(Project)
+admin.site.register(Expense)
+admin.site.register(Category)
